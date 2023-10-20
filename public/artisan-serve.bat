1abec68ec3f1dff1193d1cfab6a6cb759ba9921d
kutamz123 <andikautama034@gmail.com>
2023-10-20 16:48:48 +0700

    fix(aritsan-serve) : mengubah host artisan serve allow from all

--- public/artisan-serve.bat
+++ public/artisan-serve.bat
@@ -1,4 +1,3 @@
 @echo off
-for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 cd C:/xampp/htdocs/api-documentation-order
-php artisan serve --host=%NetworkIP%
+php artisan serve --host=0.0.0.0
