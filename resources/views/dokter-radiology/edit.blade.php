7be052bd2e72b531dcb8e9a35d6a78b8d2ec8ed6
kutamz123 <andikautama034@gmail.com>
2024-01-11 15:16:32 +0700

    edit radiologi revisi

--- resources/views/dokter-radiology/edit.blade.php
+++ resources/views/dokter-radiology/edit.blade.php
@@ -7,7 +7,7 @@
     <title>create ttd tangan dokter radiologi</title>
 </head>
 <body>
-    <iframe src="http://<?= $_SERVER['SERVER_NAME']; ?>:8000/dokter-radiology/{{ $dokterRadiology->pk }}" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%" scrolling="auto"></iframe>
+    <iframe src="http://<?= $_SERVER['HTTP_HOST']; ?>/dokter-radiology/{{ $dokterRadiology->pk }}" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%" scrolling="auto"></iframe>
     <form action="/dokter-radiology/{{ $dokterRadiology->pk }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
